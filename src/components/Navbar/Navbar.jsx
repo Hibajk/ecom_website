01451f180ecc62f30ee656af8e557b3287eff781
Hibajk <hibachenni22@gmail.com>
2024-10-13 20:27:55 +0200

    updating css and adding logic to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 
 
 const Navbar = () => {
+
+  const [menu,setMenu] = useState("Home")
+
   return (
     <div className='navbar'>
         <img src={assets.logo} alt="" className='logo'/>
         <ul className='navbar-menu'>
-          <li>Home</li>
-          <li>Menu</li>
-          <li>Mobile App</li>
-          <li>Contact us</li>
+          <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</li>
+          <li onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</li>
+          <li onClick={()=>setMenu("Mobile App")} className={menu==="Mobile App"?"active":""}>Mobile App</li>
+          <li onClick={()=>setMenu("Contact us")} className={menu==="Contact us"?"active":""}>Contact us</li>
         </ul>
         <div className='navbar-right'>
           <div className='navbar-search-icon'>
